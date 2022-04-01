d74c120d58d3eb9a0a14436061c01aa7d77ba5f8
Seyibabs <seyibabs.ng@gmail.com>
2022-04-01 20:13:57 +0100

    Student class

---
+++ classes/student.php
@@ -0,0 +1,29 @@
+<?php
+
+  class Student{
+
+      private $studentId;
+      private $sqlQuery;
+
+
+      public function __construct(){
+          $this->sqlQuery = null;
+      }
+
+      public function getStudentProfileById($DB, $studentId){
+          $this->studentId = $studentId;
+
+          $this->sqlQuery = "Select u.id, u.schoolid, u.uuid, u.surname, u.firstname, u.middlename, u.gender, u.email,
+                             u.phone, s.name, c.class from {newwaves_schools_users} u left join {newwaves_schools} s on
+                             u.schoolid=s.id left join {newwaves_schools_students} c on u.uuid=c.admission_no
+                             where u.id={$this->studentId}";
+          $studentData = $DB->get_records_sql($this->sqlQuery);
+          return $studentData;
+      }
+
+
+
+  }
+
+
+?>
