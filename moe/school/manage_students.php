51f0d933dc9ff2845a9fee73cd3a1393b71b08d6
Seyibabs <seyibabs.ng@gmail.com>
2022-04-01 20:10:20 +0100

    Manage Students

--- moe/school/manage_students.php
+++ moe/school/manage_students.php
@@ -112,7 +112,7 @@ global $DB;
 
 
 
-            $viewHref = "window.location='view_student.php?q=".mask($_GET_URL_school_id)."&u=".mask($row->id)."'";
+            $viewHref = "window.location='student/view_student.php?q=".mask($_GET_URL_school_id)."&u=".mask($row->id)."'";
             $editHref = "window.location='edit_student.php?q=".mask($_GET_URL_school_id)."&u=".mask($row->id)."'";
             $btnView = "<button onclick={$viewHref} class='btn btn-success btn-sm rounded '>View</button>";
             $btnEdit = "<button onclick={$editHref} class='btn btn-warning btn-sm rounded '>Edit</button>";
