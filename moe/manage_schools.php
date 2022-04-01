769aa59b8ace58b0dbff2f5ce234ecb4d1041f80
Seyibabs <seyibabs.ng@gmail.com>
2022-04-01 08:32:12 +0100

    Modified manage_schools.php

--- moe/manage_schools.php
+++ moe/manage_schools.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle Course Rollover Plugin
+// This file is part of Newwaves Integrator Plugin
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,8 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package     local_message
- * @author      Kristian
+ * @package     manage_schools
+ * @author      Seyibabs
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @var stdClass $plugin
  */
@@ -25,6 +25,7 @@
  require_once(__DIR__.'/../../../config.php');
  require_login();
  require_once($CFG->dirroot.'/local/newwaves/functions/schooltypes.php');
+ require_once($CFG->dirroot.'/local/newwaves/functions/state.php');
  require_once($CFG->dirroot.'/local/newwaves/functions/encrypt.php');
  require_once($CFG->dirroot.'/local/newwaves/lib/mdb.css.php');
  require_once($CFG->dirroot.'/local/newwaves/includes/page_header.inc.php');
@@ -36,12 +37,13 @@
  $PAGE->set_url(new moodle_url('/local/newwaves/moe/manage_schools.php'));
  $PAGE->set_context(\context_system::instance());
  $PAGE->set_title('Manage Schools');
+ $PAGE->set_heading('Manage Schools');
 
  echo $OUTPUT->header();
 
 
  $pageTitle = pageHeader("Manage Schools");
- echo $pageTitle;
+ //echo $pageTitle;
 
  // nav bar
  include_once($CFG->dirroot.'/local/newwaves/nav/moe_main_nav.php');
@@ -62,12 +64,13 @@
  echo "<table class='table table-stripped border' id='tblData'>";
  echo "<thead>";
  echo "<tr class='font-weight-bold' >";
-      echo "<th class='py-3'>SN</th><th>School Name</th><th>School Type</th><th>Address</th><th class='text-center'>Action</th></tr>";
+      echo "<th class='py-3'>SN</th><th>School Name</th><th>School Type</th><th>State</th><th>LGA</th><th>Address</th><th class='text-center'>Action</th></tr>";
  echo "</thead>";
  echo "<tbody>";
 
     foreach($schools as $school){
         $schoolType = schoolTypes($school->type);
+        $state = state($school->state);
 
         $viewHref = "window.location='school/schoolinfo.php?q=".mask($school->id)."'";
         $editHref = "window.location='school/edit_schoolinfo.php?q=".mask($school->id)."'";
@@ -77,6 +80,8 @@
             echo "<td class='text-center'>{$sn}.</td>";
             echo "<td>{$school->name}</td>";
             echo "<td>{$schoolType}</td>";
+            echo "<td>{$state}</td>";
+            echo "<td>{$school->lga}</td>";
             echo "<td>{$school->address}</td>";
             echo "<td class='text-center'>{$viewBtn} {$editBtn}</td>";
         echo "</tr>";
