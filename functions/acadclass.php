54bac278cd0912b9aa54bd67f0cacd5a3d37eda2
Seyibabs <seyibabs.ng@gmail.com>
2022-04-01 20:12:28 +0100

    Class resolution function

---
+++ functions/acadclass.php
@@ -0,0 +1,44 @@
+<?php
+
+  function acadclass($option){
+
+    $acadclass = '';
+
+    switch($option){
+        case 0:
+            $acadclass = "Basic 3";
+            break;
+        case 1:
+            $acadclass = "Basic 4";
+            break;
+        case 2:
+            $acadclass = "Basic 5";
+            break;
+        case 3:
+            $acadclass = "Basic 6";
+            break;
+        case 4:
+            $acadclass = "JSS 1";
+            break;
+        case 5:
+            $acadclass = "JSS 2";
+            break;
+        case 6:
+            $acadclass = "JSS 3";
+            break;
+        case 7:
+            $acadclass = "SSS 1";
+            break;
+        case 8:
+            $acadclass = "SSS 2";
+            break;
+        case 9:
+            $acadclass = "SSS 3";
+            break;
+    }
+
+      return $acadclass;
+  }
+
+
+?>
