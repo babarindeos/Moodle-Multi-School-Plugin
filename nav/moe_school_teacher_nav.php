cd01b24c2fcbf5fde747845fa307c0401422f88e
Seyibabs <seyibabs.ng@gmail.com>
2022-04-02 18:32:50 +0100

    Teacher Nav

--- nav/moe_school_teacher_nav.php
+++ nav/moe_school_teacher_nav.php
@@ -11,7 +11,7 @@
   <button onclick="window.location='<?php echo $moe_dashboard; ?>'" border='0' class='btn btn-success btn-sm'>MOE Dashboard</button>
   <button onclick="window.location='<?php echo $teacher_dashboard; ?>'" border='0' class='btn btn-info btn-sm'>Teachers Dashboard</button>
   <button onclick="window.location='<?php echo $manage_teachers; ?>'" border='0' class='btn btn-warning btn-sm'>Manage Teachers</button>
-  <button onclick="window.location='<?php echo $create_teacher; ?>'" border='0' class='btn btn-primary btn-sm'>Create Teacher</button>
+  
 </div>
 
 <hr/>
