9a4d12b5fe5b835bb122b0ad1e3cd952423dfbcc
Seyibabs <seyibabs.ng@gmail.com>
2022-04-02 04:30:41 +0100

    Modal Delete

---
+++ includes/modal_delete.inc.php
@@ -0,0 +1,25 @@
+<!-- Modal -->
+<div class="modal fade" id="deleteModalCenter" tabindex="-1" role="dialog" aria-labelledby="deleteModalCenterTitle"
+  aria-hidden="true">
+
+  <!-- Add .modal-dialog-centered to .modal-dialog to vertically center the modal -->
+  <div class="modal-dialog modal-dialog-centered" role="document">
+
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Confirm Delete</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Do you wish to delete the selected record? Please Confirm. <br/><small><strong>Note:</strong> The operation can not be reversed.</small>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-info" data-dismiss="modal">Cancel</button>
+        <button type="button" id='btn-delete-modal'  class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
