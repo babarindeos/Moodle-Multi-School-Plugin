bd5fd55e7cffd4af39af1a7270e17f6dc5f73f8f
Seyibabs <seyibabs.ng@gmail.com>
2022-04-06 17:53:53 +0100

    Classes

--- classes/form/create_school_head.php
+++ classes/form/create_school_head.php
@@ -20,11 +20,12 @@
 
         // title
         $title = array();
-        $title['0'] = 'Mr.';
-        $title['1'] = 'Mrs.';
-        $title['2'] = 'Ms.';
-        $title['3'] = 'Dr.';
-        $title['4'] = 'Prof.';
+        $title['0'] = '-- Select Title --';
+        $title['1'] = 'Mr.';
+        $title['2'] = 'Mrs.';
+        $title['3'] = 'Ms.';
+        $title['4'] = 'Dr.';
+        $title['5'] = 'Prof.';
         $mform->addElement('select', 'title', 'Title', $title);
         $mform->setDefault('title', '0');
 
@@ -50,8 +51,9 @@
 
         //gender
         $gender = array();
-        $gender[0] = 'Male';
-        $gender[1] = 'Female';
+        $gender[0] = '-- Select Gender --';
+        $gender[1] = 'Male';
+        $gender[2] = 'Female';
         $mform->addElement('select', 'gender', 'Gender', $gender);
         $mform->setDefault('gender', '0');
 
