f882e1c886f7a9df74487852b42b1485646870bf
Seyibabs <seyibabs.ng@gmail.com>
2022-04-06 12:51:06 +0100

    Adding Navigation

--- lib.php
+++ lib.php
@@ -5,10 +5,20 @@
  *
  * @param navigation_node $frontpage Node representing the front page in the navigation tree.
  */
-function newwaves_extend_navigation_frontpage(navigation_node $frontpage) {
-    $frontpage->add('newwaves',
-        new moodle_url('/local/newwaves/newwaves_dashboard.php')
+function local_newwaves_extend_navigation(global_navigation $root) {
+    // $nav->add(get_string('pluginname', 'local_newwaves'),
+    //     new moodle_url('/local/newwaves/newwaves_dashboard.php')
+    // );
+    $node = navigation_node::create(
+      get_string('pluginname', 'local_newwaves'),
+      new moodle_url('/local/newwaves/newwaves_dashboard.php'),
+      navigation_node::TYPE_CUSTOM,
+      null,
+      null,
+      new pix_icon('t/message', '')
     );
+    $node->showinflatnavigation = true;
+    $root->add_node($node);
 }
 
 //
