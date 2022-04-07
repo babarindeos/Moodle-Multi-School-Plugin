c62b3dcab4a1dd1bf61e8fc4e370460d634c5997
videx <odejinmiabraham@gmail.com>
2022-04-07 07:12:07 +0100

    move selfregistration

--- myschool/teacherselfregistration/preregistration2.php
+++ myschool/teacherselfregistration/preregistration2.php
@@ -15,16 +15,16 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package     create_school_teacher
+ * @package     create_school_student
  * @author      Seyibabs
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @var stdClass $plugin
  */
 
 
-require_once(__DIR__.'/../../../config.php');
+require_once(__DIR__ . '/../../../../config.php');
 
-require_once($CFG->dirroot.'/local/newwaves/classes/form/uploadpictures.php');
+require_once($CFG->dirroot.'/local/newwaves/classes/form/create_school_student.php');
 require_once($CFG->dirroot.'/local/newwaves/functions/schooltypes.php');
 require_once($CFG->dirroot.'/local/newwaves/functions/encrypt.php');
 require_once($CFG->dirroot.'/local/newwaves/functions/gender.php');
@@ -37,17 +37,17 @@ require_once($CFG->dirroot.'/local/newwaves/classes/auth.php');
 
 global $DB;
 
-$PAGE->set_url(new moodle_url('/local/newwaves/teacherselfregistration/preregistration.php'));
+$PAGE->set_url(new moodle_url('/local/newwaves/myschool/teacherselfregistration/preregistration.php'));
 $PAGE->set_context(\context_system::instance());
-$PAGE->set_title('Create School Teacher');
-$PAGE->set_heading('Teacher');
+$PAGE->set_title('Create School Student');
+$PAGE->set_heading('Student');
 
 
-$mform = new Uploadpictures();
+$mform = new createSchoolStudent();
 
 
 if ($mform->is_cancelled()){
-    redirect($CFG->wwwroot.'/local/newwaves/teacherselfregistration/preregistration1.php', 'No School Teacher is created. You cancelled the operation.');
+    redirect($CFG->wwwroot.'/local/newwaves/myschool/teacherselfregistration/preregistration1.php', 'No School Student is created. You cancelled the operation.');
 
 }else if($fromform = $mform->get_data()){
 
@@ -61,54 +61,57 @@ if ($mform->is_cancelled()){
 //
 //    }else{
 //
+//$title = title($fromform->title);
+//          $gender = gender($fromform->gender);
 //
-//        $recordtoinsert = new stdClass();
-//        $recordtoinsert->schoolid = $fromform->school_id;
-//        $recordtoinsert->uuid = $fromform->admission_no;
-//        $recordtoinsert->surname = $fromform->surname;
-//        $recordtoinsert->firstname = $fromform->firstname;
-//        $recordtoinsert->middlename = $fromform->middlename;
-//        $recordtoinsert->gender = $fromform->gender;
-//        $recordtoinsert->email = $fromform->email;
-//        $recordtoinsert->phone = $fromform->phone;
-//        $recordtoinsert->role = "teacher";
-//        $recordtoinsert->creator = $USER->id;
-//        $recordtoinsert->timecreated = time();
-//        $recordtoinsert->timemodified = time();
+//          $recordtoinsert = new stdClass();
+//          $recordtoinsert->schoolid = $fromform->school_id;
+//          $recordtoinsert->uuid = $fromform->staff_no;
+//          $recordtoinsert->title = $title;
+//          $recordtoinsert->surname = $fromform->surname;
+//          $recordtoinsert->firstname = $fromform->firstname;
+//          $recordtoinsert->middlename = $fromform->middlename;
+//          $recordtoinsert->gender = $gender;
+//          $recordtoinsert->email = $fromform->email;
+//          $recordtoinsert->phone = $fromform->phone;
+//          $recordtoinsert->role = "teacher";
+//          $recordtoinsert->creator = $USER->id;
+//          $recordtoinsert->timecreated = time();
+//          $recordtoinsert->timemodified = time();
 //
-//        $DB->insert_record('newwaves_schools_users', $recordtoinsert);
+//          $DB->insert_record('newwaves_schools_users', $recordtoinsert);
 //
-//        // write to moodle_users
-//        $createlogin = new stdClass();
-//        $createlogin->auth = 'manual';
-//        $createlogin->confirmed = '1';
-//        $createlogin->policyagreed = '0';
-//        $createlogin->deleted = '0';
-//        $createlogin->suspended = '0';
-//        $createlogin->mnethostid = '1';
-//        $createlogin->username = $fromform->email;
-//        $createlogin->password = md5('12345678');
-//        $createlogin->firstname = $fromform->firstname;
-//        $createlogin->lastname = $fromform->surname;
-//        $createlogin->email = $fromform->email;
+//          // write to moodle_users
+//          $createlogin = new stdClass();
+//          $createlogin->auth = 'manual';
+//          $createlogin->confirmed = '1';
+//          $createlogin->policyagreed = '0';
+//          $createlogin->deleted = '0';
+//          $createlogin->suspended = '0';
+//          $createlogin->mnethostid = '1';
+//          $createlogin->username = $fromform->email;
+//          $createlogin->password = md5('12345678');
+//          $createlogin->firstname = $fromform->firstname;
+//          $createlogin->lastname = $fromform->surname;
+//          $createlogin->email = $fromform->email;
 //
-//        $DB->insert_record("user", $createlogin);
+//          $DB->insert_record("user", $createlogin);
 //
 //
-//        // write to teacher table
-//        $createteacher = new stdClass();
-//        $createteacher->admission_no = $fromform->admission_no;
-//        $createteacher->schoolid = $fromform->school_id;
-//        $createteacher->class = $fromform->class;
-//        $createteacher->timestamp = time();
+//          //
+//          // write to teacher
+//          $createteacher = new stdClass();
+//          $createteacher->staff_no = $fromform->staff_no;
+//          $createteacher->schoolid = $fromform->school_id;
+//          $createteacher->timecreated = time();
+//          $createteacher->timemodified = time();
 //
-//        $DB->insert_record("newwaves_schools_teachers", $createteacher);
+//          $DB->insert_record("newwaves_schools_teachers", $createteacher);
 
 
-
-    $schoolinfo_href = "preregistration3.php?q=".mask(1);
-    $newTeacher = $fromform->surname.' '.$fromform->firstname;
-    redirect($CFG->wwwroot."/local/newwaves/teacherselfregistration/{$schoolinfo_href}", "A Teacher with the name <strong>{$newTeacher}</strong> has been successfully created.");
+        $schoolinfo_href = "preregistration3.php?q=".mask(1);
+        $newStudent = $fromform->surname.' '.$fromform->firstname;
+        redirect($CFG->wwwroot."/local/newwaves/myschool/studentselfregistration/{$schoolinfo_href}", "A Student with the name <strong>{$newStudent}</strong> has been successfully created.");
 //    }
 
 
@@ -118,7 +121,7 @@ if ($mform->is_cancelled()){
 }else {
     // Get School Id if not redirect page
     if (!isset($_GET['q']) || $_GET['q'] == '') {
-        redirect($CFG->wwwroot . '/local/newwaves/teacherselfregistration/preregistration1.php', 'Sorry, the page is not fully formed with the required information.');
+        redirect($CFG->wwwroot . '/local/newwaves/myschool/studentselfregistration/preregistration1.php', 'Sorry, the page is not fully formed with the required information.');
     }
     $_GET_URL_school_id = explode("-", htmlspecialchars(strip_tags($_GET['q'])));
     $_GET_URL_school_id = $_GET_URL_school_id[1];
@@ -126,8 +129,9 @@ if ($mform->is_cancelled()){
 }
 
 echo $OUTPUT->header();
-echo "<h2><small>[ School Information ]</small></h2>";
+echo "<h2>School Information <small>[ Teacher ]</small></h2>";
 $active_menu_item = "teachers";
+
 
 // retrieve school information from DB
 $sql = "SELECT * from {newwaves_schools} where id={$_GET_URL_school_id}";
@@ -146,9 +150,7 @@ foreach($school as $row){
 
 <hr/>
 
-<!-- end of navigation //-->
-
-<div class="row d-flex justify-content-right mt-4">
+<div class="row d-flex justify-content-right mt-4 mb-4">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
         <h4 class='font-weight-normal'>Create School Teacher</h4>
     </div>
@@ -158,6 +160,7 @@ foreach($school as $row){
 <div class="row border rounded py-4">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <?php
+
 
 
 
