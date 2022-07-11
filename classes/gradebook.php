c472f369753691e3a93cce835dd783196f32be64
Seyibabs <seyibabs.ng@gmail.com>
2022-07-11 16:41:02 +0100

    Gradebook class

--- classes/gradebook.php
+++ classes/gradebook.php
@@ -24,6 +24,19 @@ class GradeBook{
       return $this->result;
   }
 
+
+  public function getGradeBookItemByItemId($DB, $item_id){
+      $this->sqlQuery = "Select gi.id, gi.courseid, gi.categoryid, gi.itemname, gi.itemtype, gi.itemmodule, gi.iteminstance,
+                         gi.itemnumber, gi.iteminfo, gi.idnumber, gi.calculation, gi.gradetype, gi.grademax, gi.grademin,
+                         gi.scaleid, gi.outcomeid, gi.gradepass, gi.multfactor, gi.aggregationcoef, gi.aggregationcoef2,
+                         gi.sortorder, gi.display, gi.decimals, gi.hidden, gi.locked, gi. locktime, gi.needsupdate, gi.weightoverride,
+                         gi.timecreated, gi.timemodified, nw.full_name, nw.short_code from {grade_items} gi inner join {newwaves_course} nw on
+                         gi.courseid=nw.mdl_course_id where gi.id={$item_id} ";
+      $this->output = $DB->get_records_sql($this->sqlQuery);
+      $this->result = $this->output;
+      return $this->result;
+  }
+
 }
 
 
