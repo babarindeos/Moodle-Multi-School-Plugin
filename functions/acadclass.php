9d899af7212848e56a0fd1ca39c971135439d884
Seyibabs <seyibabs.ng@gmail.com>
2022-07-14 14:35:06 +0100

    Acada Class Function modified

--- functions/acadclass.php
+++ functions/acadclass.php
@@ -5,35 +5,38 @@
     $acadclass = '';
 
     switch($option){
-        case 0:
+        case 1:
             $acadclass = "Basic 3";
             break;
-        case 1:
+        case 2:
             $acadclass = "Basic 4";
             break;
-        case 2:
+        case 3:
             $acadclass = "Basic 5";
             break;
-        case 3:
+        case 4:
             $acadclass = "Basic 6";
             break;
-        case 4:
+        case 5:
             $acadclass = "JSS 1";
             break;
-        case 5:
+        case 6:
             $acadclass = "JSS 2";
             break;
-        case 6:
+        case 7:
             $acadclass = "JSS 3";
             break;
-        case 7:
+        case 8:
             $acadclass = "SSS 1";
             break;
-        case 8:
+        case 9:
             $acadclass = "SSS 2";
             break;
-        case 9:
+        case 10:
             $acadclass = "SSS 3";
+            break;
+        case 11:
+            $acadclass = "Not Applicable";
             break;
     }
 
